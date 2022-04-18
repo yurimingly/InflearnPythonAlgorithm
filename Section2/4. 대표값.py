0690339e109bc17c5627bc915e744ba311f6a5a4
yurimingly <yuyu9895@likelion.org>
2022-04-18 22:59:20 +0900

    202204182300

---
+++ Section2/4. 대표값.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open("input.txt","rt")
+
+n = int(input())
+a = list(map(int, input().split()))
+ave = round(sum(a)/n)
+min = 2147000000
+minx = 100000
+
+for idx, x in enumerate(a):
+    tmp = abs(x-ave)
+    if tmp < min:
+        min = tmp
+        score =x
+        res=idx+1
+    elif tmp == min:
+        if x>score:
+            score = x
+            res = idx+1
+print(ave,res)
